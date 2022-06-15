b69a44afffdd7390e9ad1a9c8b6190e08362db8b
Vitaliy Dyachuk <shadespiritenator@gmail.com>
2022-06-16 00:35:34 +0300

    cleanup

--- index.js
+++ index.js
@@ -19,23 +19,19 @@ import serveStatic from 'serve-static';
 const fs = await import("fs");
 import { resolve } from "path";
 
-import state from "./middleware/state.js";
 import applyAuthMiddleware from "./middleware/auth.js";
 import verifyRequest from "./middleware/verify-request.js";
 import topLevelAuthRedirect from "./helpers/top-level-auth-redirect.js";
 
 const log = console.log;
 
-//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
-//const host = '127.0.0.1';
 const port = 3000;
 const USE_ONLINE_TOKENS = true;
 const TOP_LEVEL_OAUTH_COOKIE = "shopify_top_level_oauth";
 
 const { SHOPIFY_API_KEY, SHOPIFY_API_SECRET, SHOPIFY_API_SCOPES, HOST, SHOP } = process.env;
 
-// const shops = {};
-// const ACTIVE_SHOPIFY_SHOPS = {};
+import state from "./middleware/state.js";
 const shops = state.shops;
 const ACTIVE_SHOPIFY_SHOPS = state.ACTIVE_SHOPIFY_SHOPS;
 
@@ -53,7 +49,6 @@ log('Set: ', set);
 Shopify.Context.initialize(set);
 
 const app = express();
-// app.use(cookieParser())
 app.set("top-level-oauth-cookie", TOP_LEVEL_OAUTH_COOKIE);
 app.set("active-shopify-shops", ACTIVE_SHOPIFY_SHOPS);
 // app.set("use-online-tokens", USE_ONLINE_TOKENS);
@@ -68,25 +63,6 @@ app.get("/session-info", async (req, res) => {
         shops
     })
 });
-
-/*
---- url from iframe in admin page--- sometime not working !!!
-https://oauth-shopify-app.herokuapp.com/?
-hmac = f96f9e80c327e56790cfae9d0c1463336abd67f9dc7be7c8105c93bc5381f29c &
-host = dG9ueWpvc3Mtc3RvcmUubXlzaG9waWZ5LmNvbS9hZG1pbg &
-locale = en &
-session = 7995fcfaea4e454672011963028f2804bf5361c621262535a809ebef65396b85 &
-shop = tonyjoss-store.myshopify.com &
-timestamp = 1654801115
-
---- but this url also works --- better !!!
-https://oauth-shopify-app.herokuapp.com/auth?
-shop = tonyjoss-store.myshopify.com
-
---- sometimes redirect to !!! --- ned authorization in account !!!
-https://accounts.shopify.com/lookup?
-rid = 7fc0f07d-2026-48f9-bfed-398fdf861178
-*/
 
 app.get("/", (req, res) => {
     const isShop = typeof shops[req.query.shop] !== 'undefined';
@@ -114,6 +90,7 @@ app.post("/webhooks", async (req, res) => {
 });
 
 app.use(express.json());
+
 app.use((req, res, next) => {
     const shop = req.query.shop;
     if (Shopify.Context.IS_EMBEDDED_APP && shop) {
@@ -163,7 +140,6 @@ app.get("/api/products", async (req, res) => {
     }
 });
 
-
 // app.get("api/products-count", verifyRequest(app), async (req, res) => {
 app.get("/api/products-count", async (req, res) => {
     const shop = req.query.shop;
@@ -188,8 +164,6 @@ app.post("/graphql", async (req, res) => {
 
 app.use(compression());
 app.use(serveStatic(resolve("app-front/dist")));
-
-
 
 app.use("/*", (req, res, next) => {
     // Client-side routing will pick up on the correct route to render, so we always render the index here
