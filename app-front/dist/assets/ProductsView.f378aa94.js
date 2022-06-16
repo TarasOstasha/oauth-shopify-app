8d745f83733cdcf793f7698885bf6fddaa08b5a3
Vitaliy Dyachuk <shadespiritenator@gmail.com>
2022-06-16 18:16:30 +0300

    build

--- app-front/dist/assets/ProductsView.f378aa94.js
+++
@@ -1,1 +0,0 @@
-import{d as e,o,c as t}from"./index.adb4572b.js";const s=e({name:"ProductsView",setup(n){return(a,c)=>(o(),t("main"))}});export{s as default};
---
+++ app-front/dist/assets/logo.7bb07e2a.js
@@ -0,0 +1,1 @@
+var s="/assets/logo.da9b9095.svg";export{s as _};
